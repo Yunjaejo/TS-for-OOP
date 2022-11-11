b98b13ffc2a76978eb742f545b94433fd4898a67
Yunjaejo <yunjae.dev@gmail.com>
2022-11-11 15:25:45 +0900

    style: 오타 수정

--- BASIC/DiscriminatedUnion.ts
+++ BASIC/DiscriminatedUnion.ts
@@ -1,5 +1,5 @@
 {
-  // Discriminated Union : tpye Success 와 Fail 에 공통된 속성 result 를 강제함으로 라인 25에서 분기처리 간편하고 직관적이게 가능함.
+  // Discriminated Union : type Success 와 Fail 에 공통된 속성 result 를 강제함으로 라인 25에서 분기처리 간편하고 직관적이게 가능함.
   type Login = Success | Fail
   type Success = {
     result: 'Success',
